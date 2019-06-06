673aa363fbcf50e348d7cafbfbf541ef0953d0f6
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-06-06 09:54:36 +0530

    Add files via upload

---
+++ angularweekend/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'HomeController'
+  })
+
+  .when('/blog', {
+    templateUrl : 'pages/blog.html',
+    controller  : 'BlogController'
+  })
+
+  .when('/about', {
+    templateUrl : 'pages/about.html'
+     })
+  .when('/services', {
+    templateUrl : 'pages/services.html'
+     })
+
+  .otherwise({redirectTo: '/'});
+});
+
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Hello from HomeController';
+});
+
+app.controller('BlogController', function($scope) {
+  $scope.message = 'Hello from BlogController';
+});
+
+app.controller('AboutController', function($scope) {
+  $scope.message = 'Hello from AboutController';
+});
